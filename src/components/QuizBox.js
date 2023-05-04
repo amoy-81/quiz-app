1c42b2ebbdbaac71fcad7ea5f100fcb34697aa77
Ali <ali.moaydi2020@gmail.com>
2023-05-04 18:09:48 +0430

    2. create Quiz Box

--- src/components/QuizBox.js
+++ src/components/QuizBox.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React , { useContext, useState } from 'react'
 
-export default function () {
+// Styles
+import styles from "../styles/QuizBox.module.css";
+
+export default function ({data}) {
+  const [click , setClick] = useState("default");
+  const clickHandler = (event) => {
+    if (event) {
+      setClick("GREEN")
+    } else {
+      setClick("RED")
+    }
+  }
   return (
-    <div></div>
+    <div className={styles.container}>
+      <div className={styles.quizHead}>
+        <h3>{data.quiz}</h3>
+      </div>
+      <div className={styles.answersBox}>
+        {
+          data.answers.map(answer => <p key={answer.id} onClick={() => clickHandler(answer.val)} className={click === "GREEN" && answer.val ? styles.green : "" ||click === "RED" && !answer.val ? styles.red : "" ||click === "RED" && answer.val ? styles.green : "" }>{answer.text}</p>)
+        }
+      </div>
+      <div >
+
+      </div>
+    </div>
   )
 }
--- src/components/Quizs.js
+++ src/components/Quizs.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react'
 import { ContextData } from '../context/Context.Provider'
+import QuizBox from './QuizBox';
 
 export default function Quizs() {
   const {data , dispatch} = useContext(ContextData);
-  return (
-    <div >
-        
+  const [qNum , setQNum] = useState(3);
+    return (
+    <div style={{display:"flex" , justifyContent: "center" , alignItems : "center" , padding : "20px"}} >
+        <QuizBox data={data.quizs[qNum]}/>
     </div>
   )
 }
